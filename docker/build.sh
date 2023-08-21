482dafc9ec75cbf63d2e421293ccc9d041d0d9f0
kamalteja <kamalteja.gurramkonda@gmail.com>
2023-08-21 19:46:03 +0000

    WIP#2: Dockerise app

--- docker/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-pushd $SCRIPT_DIR/.. &> /dev/null
-python -m piptools compile --resolver=backtracking --strip-extras -o requirements.txt pyproject.toml
-popd
-
-# Build containers
-docker-compose -f $SCRIPT_DIR/docker-compose.yaml build
--- wsgi.py
+++
@@ -1,6 +0,0 @@
-from hello import app
-
-
-if __name__ == "__main__":
-    # app.config["DEBUG"] = True
-    app.run(debug=False, port=8000)
