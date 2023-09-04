e0176e8149914062dcad8e8825a3880dea161861
Kamal Gurramkonda <kamalteja.gurramkonda@gmail.com>
2023-09-05 00:51:08 +0200

    Added POST request validation

--- notifier/earth_bot.py
+++ notifier/earth_bot.py
@@ -1,12 +1,17 @@
+"""Earth bot for slack commands"""
+
+from datetime import datetime
+import hashlib
+import hmac
+
 import logging
 import os
 
 from flask import Flask, Response, render_template, request
-from freerider.arguments import rider_arguments
-from freerider.plugins.hertz import hertz_rides
 
 from notifier import get_static_dir, get_template_dir
 from notifier.lib.slack.client import SlackCommandException, SlackForm
+from notifier.ride.frider import get_ride_details
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=os.environ.get("SLACK_BOT_LOGLEVEL", "INFO"))
@@ -16,10 +21,49 @@ earth_app = Flask(
 )
 
 
+@earth_app.before_request
+def validate_request():
+    """Validate requests from SLACK"""
+    if request.method == "GET":
+        return
+
+    log.info(
+        "Verifying reqeust -> method: %s, url: %s, path: %s",
+        request.method,
+        request.url,
+        request.path,
+    )
+
+    # Validating timestamp of request to be less than 5minutes.
+    timestamp = request.headers["X-Slack-Request-Timestamp"]
+    if (
+        datetime.now() - datetime.fromtimestamp(int(timestamp))
+    ).total_seconds() > 60 * 5:
+        # The request timestamp is more than five minutes from local time.
+        # It could be a replay attack, so let's ignore it.
+        raise SlackCommandException("request timestamp is more than five minutes")
+
+    # Verifying the POST request against signing key
+    raw_body = request.get_data().decode()
+    if not raw_body:
+        raise SlackCommandException("Received Emply POST body")
+
+    base_string = str.encode(f"v0:{timestamp}:{raw_body}")
+    my_signature_digest = hmac.new(
+        key=os.environ["SLACK_APP_SIGNING_KEY"].encode("UTF-8"),
+        msg=base_string,
+        digestmod=hashlib.sha256,
+    ).hexdigest()
+
+    if not hmac.compare_digest(
+        f"v0={my_signature_digest}", request.headers["X_SLACK_SIGNATURE"]
+    ):
+        raise SlackCommandException(f"Invalid Signature for path: '{request.url}'")
+
+
 @earth_app.route("/", methods=["GET"])
 def hello():
     """Welcome message for the slack-bot app"""
-    print(get_template_dir())
     return Response(render_template("earth.html"), 200)
 
 
@@ -29,39 +73,8 @@ def ride():
     Correlates to /ride slash command
     Looks up requested rides against the rider scrapper
     """
-
-    def get_rider_args(data: str):
-        stations = data.split(":")
-        if len(stations) > 2:
-            raise SlackCommandException(f"Invalid argument supplied: {data}")
-
-        if data.startswith(":"):
-            return ["--to", data[1:]]
-
-        if data.endswith(":"):
-            return ["--from", data[:-1]]
-
-        if ":" not in data:
-            return ["-s", data]
-
-        return ["--from", stations[0], "--to", stations[1]]
-
     slack_form = SlackForm(**request.form.to_dict())
-    if slack_form.command != "/ride":
-        raise SlackCommandException(f"Unknown slash command : {slack_form.command}")
-
-    ride_details = []
-    args = get_rider_args(slack_form.text)
-    log.debug("Arguments to rider: %s", args)
-
-    for free_ride in hertz_rides(rider_arguments(args)):
-        ride_details.append(str(free_ride))
-
-    if not ride_details:
-        ride_details = [f"No matching rides found for '{slack_form.text}'"]
-
-    log.debug("Ride details: %s", ride_details)
-    return Response("\n".join(ride_details), 200)
+    return Response(get_ride_details(slack_form.text), 200)
 
 
 @earth_app.route("/notify-ride", methods=["POST"])
